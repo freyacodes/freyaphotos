401ad3f534cf9da389e00db12b1dca4c6f0cd995
Freya Arbjerg <freya@arbjerg.dev>
2023-07-20 18:33:38 +0200

    Update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	kotlin("jvm") version "1.8.21"
-	kotlin("plugin.serialization") version "1.8.21"
+	kotlin("jvm") version "1.9.0"
+	kotlin("plugin.serialization") version "1.9.0"
 	application
 }
 
@@ -23,7 +23,7 @@ repositories {
 dependencies {
 	implementation("org.jsoup:jsoup:1.16.1")
 	implementation("com.github.mjeanroy:exiftool-lib:3.0.0")
-	implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.0")
+	implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.1")
 }
 
 tasks.withType<KotlinCompile> {
