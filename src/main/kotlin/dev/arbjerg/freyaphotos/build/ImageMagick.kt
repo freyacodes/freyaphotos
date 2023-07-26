efecd880481e8d16a6525c4bf936758983c426ef
Freya Arbjerg <freya@arbjerg.dev>
2023-07-26 16:14:59 +0200

    Switch to convert command

--- src/main/kotlin/dev/arbjerg/freyaphotos/build/ImageMagick.kt
+++ src/main/kotlin/dev/arbjerg/freyaphotos/build/ImageMagick.kt
@@ -6,7 +6,7 @@ import java.util.concurrent.TimeUnit
 object ImageMagick {
 
     fun createSmallThumbnail(inPath: Path, outPath: Path) {
-        val command = "magick $inPath -verbose -delete 1 -gravity center -scale 400x400^ -extent 400x400 $outPath".split(" ")
+        val command = "convert $inPath -verbose -delete 1 -gravity center -scale 400x400^ -extent 400x400 $outPath".split(" ")
         val proc = ProcessBuilder(command)
             .redirectError(ProcessBuilder.Redirect.INHERIT)
             .inheritIO()
