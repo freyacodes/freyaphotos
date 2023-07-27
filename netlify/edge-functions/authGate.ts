116eef0816fac3f70b0e8f8d2ae774ca4c7f2b59
Freya Arbjerg <freya@arbjerg.dev>
2023-07-27 23:30:21 +0200

    Make auth gate recognise image URLs

--- netlify/edge-functions/authGate.ts
+++ netlify/edge-functions/authGate.ts
@@ -3,6 +3,7 @@ import jwtHelper from "../JwtHelper.ts"
 import config from '../config.json' assert { type: "json" }
 
 const galleryRegex = /\/gallery\/(\w+)/;
+const galleryImageRegex = /\/img\/(\w+).*/;
 
 export default async (request: Request, context: Context) => {
     const denied = new Response("Access denied", {
@@ -16,13 +17,16 @@ export default async (request: Request, context: Context) => {
     }
 
     const url = new URL(request.url);
-    const groups = url.pathname.match(galleryRegex);
-    if (groups == null) {
+    const groups1 = url.pathname.match(galleryRegex);
+    const groups2 = url.pathname.match(galleryImageRegex);
+    if (groups1 == null && groups2 == null) {
         console.log("Access denied: Unrecognised url")
         return denied;
     }
+
+    const gallery = groups1 != null ? groups1[1] : groups2![2]
     
-    const auth = await isAuthorised(jwt, groups[1]);
+    const auth = await isAuthorised(jwt, gallery);
     if (!auth) {
         console.log("Access denied: Not authenticated or not authorised")
         return denied;
